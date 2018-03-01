ebbca17d2b3a22e9af60e684439e79a8f043361b
Daniel Roberts <droberts@dimagi.com>
2018-03-01 08:44:47 +0530

    clean up setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ setup(
     version='1.0.0',
     description="A simple memoization decorator that's also memory efficient on instance methods",
     long_description="",
+    url="https://github.com/dimagi/memoized",
+    author='Dimagi',
+    author_email='dev@dimagi.com',
     license='BSD-3',
     packages=find_packages('.'),
     install_requires=(),
     test_requires=(
-        'nose==1.3.7',
+        'nose',
     )
 )
